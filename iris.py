f5f1ebc371ee903ddbd073cab7ede22c240791d3
immanuelsavio <immanuelsavio@gmail.com>
2018-06-13 17:24:52 +0400

    just to check

--- iris.py
+++ iris.py
@@ -4,24 +4,5 @@ import pandas as pd
 import numpy as np 
 import csv
 import matplotlib.pyplot as plt 
-
-data1=open('IRIS_Dataset/iris.csv','r')
-data2=open('IRIS_Dataset/bezdekIris.csv','r')
-FormattedData=list(csv.reader(data1))	#changing raw data to an organised list
-#print(FormattedData)
-
-NData = []
-
-for EachLength in FormattedData[1:len(FormattedData)]:
-	SepalLength = []
-	SepalWidth = []
-	PetalLength = []
-	PetalWidth = []
-	SepalLength.append(float(EachLength[EachLength][0]))
-	SepalWidth.append(float(EachLength[1]))
-	PetalLength.append(float(EachLength[2]))
-	PetalWidth.append(float(EachLength[3]))
-
-#for length in SepalLength[0:len(SepalLength)]:
-print(SepalLength[1])
+import scipy sp
 

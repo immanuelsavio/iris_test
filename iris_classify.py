105d1db6c40ebd47e2a44c9f104f39f1a6bb1b55
immanuelsavio <immanuelsavio@github.com>
2018-12-08 22:03:57 +0530

    Create iris_classify.py
    New file created to classify the iris dataset

---
+++ iris_classify.py
@@ -0,0 +1,10 @@
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+import pandas as pd 
+
+excel_file = 'IRIS_Dataset/iris.csv'
+iris = pd.read_csv(excel_file)
+y = iris.species
+x=iris.drop('species',axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2)
+print("x_train:")

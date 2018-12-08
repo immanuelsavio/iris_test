8cbac42fd6d80fdb32980d916137fcdbc9996523
immanuelsavio <immanuelsavio@github.com>
2018-12-08 22:34:26 +0530

    Update iris_classify.py

--- iris_classify.py
+++ iris_classify.py
@@ -7,4 +7,5 @@ iris = pd.read_csv(excel_file)
 y = iris.species
 x=iris.drop('species',axis=1)
 x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2)
-print("x_train:")
+arr_train
+for i in x_train(1:length(x_train))

5a55dae9c6e8fea529a39d2ec8114aaaf56e61a2
immanuelsavio <immanuelsavio@github.com>
2018-12-10 17:26:31 +0530

    Update iris_classify.py

--- iris_classify.py
+++ iris_classify.py
@@ -1,4 +1,5 @@
 from sklearn import tree
+
 from sklearn.model_selection import train_test_split
 import pandas as pd 
 
@@ -7,5 +8,10 @@ iris = pd.read_csv(excel_file)
 y = iris.species
 x=iris.drop('species',axis=1)
 x_train, x_test, y_train, y_test = train_test_split(x, y,test_size=0.2)
-arr_train
-for i in x_train(1:length(x_train))
+x_train_arr=[]
+x_test_arr=[]
+y_train_arr=[]
+x_test_arr=[]
+
+for i in file(iris):
+    row = x.split(',')
